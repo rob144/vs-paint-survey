fbb0ac0dcbc44ba4253f2fde338aa5ff4558bd0a
Robin Dunn <mr.robin.dunn@gmail.com>
2015-08-03 16:30:39 -0700

    Adding db context and initializer.

---
+++ PaintSurvey/PaintSurvey/DAL/PaintSurveyContext.cs
@@ -0,0 +1,21 @@
+﻿using PaintSurvey.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PaintSurvey.DAL
+{
+    public class PaintSurveyContext : DbContext
+    {
+        public PaintSurveyContext() : base("PaintSurveyContext") { }
+        
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Paint> Paints { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<DefaultRoom> DefaultRooms { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- PaintSurvey/PaintSurvey/Models/Project.cs
+++ PaintSurvey/PaintSurvey/Models/Project.cs
@@ -8,8 +8,8 @@ namespace PaintSurvey.Models
     public class Project
     {
         public int key { get; set; }
-        public string username { get; set; }
         public string title { get; set; }
+        public string username { get; set; }
         public DateTime dateCreated { get; set; }
         public virtual ICollection<Room> rooms { get; set; } //1 to m Relationship to Room model
         public virtual ICollection<Room> paints { get; set; } //1 to m Relationship to Paint model
